2e625970f627932d45027ef0e855bded75e1dbbb
britne <britnesissom@gmail.com>
2020-05-30 20:34:10 -0500

    Initial Commit

---
+++ Ninja Timer Controls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ninja Timer Controls
+//
+//  Created by Brit'ne Sissom on 5/30/20.
+//  Copyright © 2020 Brit'ne Sissom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
