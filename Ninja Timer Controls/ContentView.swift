19b06878993653f0c2b1634199230e2c6ac8e525
britne <britnesissom@gmail.com>
2021-01-19 00:50:16 -0600

    updated ContentView and SettingsView

--- Ninja Timer Controls/ContentView.swift
+++ Ninja Timer Controls/ContentView.swift
@@ -7,11 +7,9 @@
 //
 
 import SwiftUI
-import Combine
 
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
-    @ObservedObject var settings = UserSettings()
     @ObservedObject var bluetooth = BLEControl.shared
     @State private var score = 0
     
@@ -36,77 +34,44 @@ struct ContentView: View {
         score = 0
     }
     
-    func viewSettings() {
-    
-    }
-    
     var body: some View {
-    NavigationView {
-        ZStack {
-            Color("AppColors").edgesIgnoringSafeArea(.all)
-            VStack {
-                
-                
-//                if !bluetooth.isEnabled {
-//                    Text("Please enable Bluetooth in your Settings to use this app")
-//                        .frame(maxWidth: .infinity)
-//                        .padding(EdgeInsets(top: 30, leading: 10, bottom: 20, trailing: 10))
-//                } else if !bluetooth.isConnected {
-//                    Text("You must connect to the Ninja Timer via Bluetooth to use this app")
-//                        .frame(maxWidth: .infinity)
-//                        .padding(EdgeInsets(top: 30, leading: 10, bottom: 20, trailing: 10))
-//                    Button(action: connectBLE) {
-//                        Text("CONNECT")
-//                        .fontWeight(.semibold)
-//                        .frame(width: 150.0, height: 50)
-//                        .background(Color.customGreen)
-//                        .foregroundColor(Color.black)
-//                        .cornerRadius(8)
-//                        .overlay(RoundedRectangle(cornerRadius: 8)
-//                            .stroke(Color.customGreen, lineWidth: 1))
-//                    }
-//                } else {
-                    VStack(spacing: 40) {
-                        Button(action: start) {
-                            Text("START")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 30))
-                            .frame(width: 150.0, height: 50)
-                            .background(Color.customGreen)
-                            .foregroundColor(Color.black)
-                            .cornerRadius(8)
-                            .overlay(RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.customGreen, lineWidth: 1))
-                        }
-                        Button(action: stop) {
-                            Text("STOP")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 30))
-                            .frame(width: 150.0, height: 50)
-                            .background(Color.stopRed)
-                            .foregroundColor(Color.black)
-                            .cornerRadius(8)
-                            .overlay(RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.stopRed, lineWidth: 1))
-                        }
+        if !bluetooth.isEnabled {
+            VStack(spacing: 40) {
+                Text("Ninja Timer Controls")
+                    .font(.system(size: 30))
+                    .fontWeight(.bold)
+                Text("Please enable Bluetooth in your Settings to use this app")
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding(EdgeInsets(top: 30, leading: 10, bottom: 20, trailing: 10))
+        } else
+        if !bluetooth.isConnected {
+            VStack(spacing: 40) {
+                Text("Ninja Timer Controls")
+                    .font(.system(size: 30))
+                    .fontWeight(.bold)
+                Text("You must connect to the Ninja Timer via Bluetooth to use this app")
+                Button(action: connectBLE) {
+                    Text("CONNECT")
+                    .fontWeight(.semibold)
+                    .frame(width: 150.0, height: 50)
+                    .background(Color.customGreen)
+                    .foregroundColor(Color.black)
+                    .cornerRadius(8)
+                    .overlay(RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.customGreen, lineWidth: 1))
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding(EdgeInsets(top: 30, leading: 10, bottom: 20, trailing: 10))
+        } else {
+            NavigationView {
+                ZStack {
+                    Color("AppColors").edgesIgnoringSafeArea(.all)
+                    VStack {
                         
-                        Button(action: reset) {
-                            Text("RESET")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 30))
-                            .frame(width: 150.0, height: 50)
-                            .background(Color.resetGold)
-                            .foregroundColor(Color.black)
-                            .cornerRadius(8)
-                            .overlay(RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.resetGold, lineWidth: 1))
-                        }
-                        
-                        Divider().padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                        
-                        HStack {
-                            Button(action: pass) {
-                                Text("CLEAR")
+                        VStack(spacing: 40) {
+                            Button(action: start) {
+                                Text("START")
                                 .fontWeight(.semibold)
                                 .font(.system(size: 30))
                                 .frame(width: 150.0, height: 50)
@@ -116,74 +81,60 @@ struct ContentView: View {
                                 .overlay(RoundedRectangle(cornerRadius: 8)
                                     .stroke(Color.customGreen, lineWidth: 1))
                             }
+                            Button(action: stop) {
+                                Text("STOP")
+                                .fontWeight(.semibold)
+                                .font(.system(size: 30))
+                                .frame(width: 150.0, height: 50)
+                                .background(Color.stopRed)
+                                .foregroundColor(Color.black)
+                                .cornerRadius(8)
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.stopRed, lineWidth: 1))
+                            }
+                            
+                            Button(action: reset) {
+                                Text("RESET")
+                                .fontWeight(.semibold)
+                                .font(.system(size: 30))
+                                .frame(width: 150.0, height: 50)
+                                .background(Color.resetGold)
+                                .foregroundColor(Color.black)
+                                .cornerRadius(8)
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.resetGold, lineWidth: 1))
+                            }
+                            
+                            Divider().padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+                            
+                            HStack {
+                                Button(action: pass) {
+                                    Text("CLEAR")
+                                    .fontWeight(.semibold)
+                                    .font(.system(size: 30))
+                                    .frame(width: 150.0, height: 50)
+                                    .background(Color.customGreen)
+                                    .foregroundColor(Color.black)
+                                    .cornerRadius(8)
+                                    .overlay(RoundedRectangle(cornerRadius: 8)
+                                        .stroke(Color.customGreen, lineWidth: 1))
+                                }
+                            }
+                            
+                            Text("Score: " + String(score))
+                                .font(.system(size: 30))
                         }
-                        
-                        Text("Score: " + String(score))
-                            .font(.system(size: 30))
-                        
-                        Divider().padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                        
-                        Toggle(isOn: $settings.isLaserEnabled) {
-                            Text("Laser Sensor").font(.system(size: 20))
-                        }.padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
-
-//                    }
-//                    .padding(.top, 20)
-                }.navigationBarTitle("Ninja Timer Controls")
-                    .navigationBarItems(trailing:
-                        Image(systemName: "gearshape.fill")
-                                .font(.system(size: 30.0))
-                                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                    )
+                        .padding(.top, 20)
+                    }
+                }
             }
+            .navigationBarTitle("Ninja Timer Controls")
+            .navigationBarItems(trailing:
+                Image(systemName: "gearshape.fill")
+                        .font(.system(size: 30.0))
+                        .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+            )
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-            //.padding(.top, 30)
-        }
-        }
-    }
-
-}
-
-@propertyWrapper
-struct UserDefault<T> {
-    let key: String
-    let defaultValue: T
-
-    init(_ key: String, defaultValue: T) {
-        self.key = key
-        self.defaultValue = defaultValue
-    }
-
-    var wrappedValue: T {
-        get {
-            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: key)
-        }
-    }
-}
-
-final class UserSettings: ObservableObject {
-    var bluetooth: BLEControl
-    let objectWillChange = PassthroughSubject<Void, Never>()
-    
-    init() {
-        bluetooth = BLEControl.shared
-    }
-
-    @UserDefault("isLaserEnabled", defaultValue: true)
-    var isLaserEnabled: Bool {
-        willSet {
-            objectWillChange.send()
-        }
-        didSet {
-            // send value to arduino via bluetooth
-            if (isLaserEnabled) {
-                bluetooth.writeValue(withValue: "y".data(using: .utf8)!)
-            } else {
-                bluetooth.writeValue(withValue: "n".data(using: .utf8)!)
-            }
         }
     }
 }
--- Ninja Timer Controls/SettingsView.swift
+++ Ninja Timer Controls/SettingsView.swift
@@ -7,15 +7,72 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SettingsView: View {
+    @ObservedObject var settings = UserSettings()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Toggle(isOn: $settings.isLaserEnabled) {
+                    Text("Laser Sensor").font(.system(size: 20))
+                }.padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+        }
+        .navigationBarTitle("Settings", displayMode: .inline)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        .padding(.top, 30)
+    }
+}
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+final class UserSettings: ObservableObject {
+    var bluetooth: BLEControl
+    let objectWillChange = PassthroughSubject<Void, Never>()
+    
+    init() {
+        bluetooth = BLEControl.shared
+    }
+
+    @UserDefault("isLaserEnabled", defaultValue: true)
+    var isLaserEnabled: Bool {
+        willSet {
+            objectWillChange.send()
+        }
+        didSet {
+            // send value to arduino via bluetooth
+            if (isLaserEnabled) {
+                bluetooth.writeValue(withValue: "y".data(using: .utf8)!)
+            } else {
+                bluetooth.writeValue(withValue: "n".data(using: .utf8)!)
+            }
+        }
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        Group {
+            //SettingsView().environment(\.colorScheme, .dark)
+            SettingsView()
+        }
     }
 }
